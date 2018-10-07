543cac07ea7cc7ac977463db14e74c0e2cfe4280
Nguyễn Bảo Quốc <quocyoghurt@gmail.com>
2018-10-07 06:59:05 +0000

    static_page

---
+++ db/schema.rb
@@ -0,0 +1,62 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20181006113455) do
+
+  create_table "bird_images", force: :cascade do |t|
+    t.string   "image"
+    t.integer  "bird_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "birds", force: :cascade do |t|
+    t.string   "bird_name"
+    t.text     "bird_info"
+    t.integer  "bird_price"
+    t.string   "bird_voice"
+    t.integer  "species_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "rating"
+    t.integer  "user_id"
+    t.integer  "bird_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "species", force: :cascade do |t|
+    t.string   "species_name"
+    t.text     "species_info"
+    t.integer  "min_price"
+    t.integer  "max_price"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "user_name"
+    t.string   "user_password"
+    t.string   "user_mail"
+    t.string   "user_role"
+    t.string   "integer"
+    t.string   "user_avatar"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+end

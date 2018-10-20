a59e1670db97b532b39fac938e29a7a9bc1757b7
Hung912 <hunghusthedspi@gmail.com>
2018-10-20 08:58:26 +0700

    Gop user migrate thanh 1 file

--- db/migrate/20181013135339_add_more_fields_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddMoreFieldsToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :birthdate, :datetime
-    add_column :users, :sex, :string
-    add_column :users, :phone, :string
-  end
-end

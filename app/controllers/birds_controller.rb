53b9d6bafef0888e504a9ab4e293b1ccc394cf8f
BinhDV <dangbinh1996914@gmail.com>
2018-10-21 12:58:03 +0700

    fix pagination

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,7 +4,7 @@ class BirdsController < ApplicationController
   # GET /birds
   # GET /birds.json
   def index
-    @birds = Bird.all.page(params[:page]).per(6)
+    @birds = Bird.all.order(:species_id).page(params[:page]).per(6)
     @bird_images = BirdImage.joins(:bird)
   end
 

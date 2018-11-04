9c65a61b59fbc1541d65063ae7f17ada848b5d03
Nguyen Tuan Anh <anhnt796@gmail.com>
2018-11-04 21:56:58 +0700

    Review done

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,13 +1,25 @@
 class Bird < ActiveRecord::Base
-    belongs_to  :species
-    has_many    :bird_images,   dependent: :destroy
-    has_many    :reviews,       dependent: :destroy
+  belongs_to  :species
+  has_many    :bird_images,   dependent: :destroy
+  has_many    :reviews,       dependent: :destroy
 
-    def rating
-      if reviews.empty?
-        0
-      else
-        (reviews.average(:rating)/10).round(1)
-      end
+  def rating
+    if reviews.empty?
+      0
+    else
+      (reviews.average(:rating)/10).round(1)
     end
+  end
+
+  def review_user_present? id
+    reviews.find_by(user: id).present?
+  end
+
+  def review_user id
+    reviews.find_by(user: id)
+  end
+
+  def review_count
+    reviews.count
+  end
 end

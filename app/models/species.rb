1bb7d132d577e3502412b35e395cbce75555b8de
Nguyễn Bảo Quốc <quocyoghurt@gmail.com>
2018-11-05 17:14:56 +0000

    show species

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,3 +1,27 @@
 class Species < ActiveRecord::Base
     has_many    :birds, dependent: :destroy
+
+    def rating
+      rate = 0
+      sum = 0
+      count = 0
+      birds.each do |bird|
+        if bird.rating != 0
+          sum = sum + bird.rating
+          count = count + 1
+        end
+      end
+      if count > 0
+        rate = (sum/count).round(1)
+      end
+      rate
+    end
+
+    def review_count
+      sum = 0
+      birds.each do |bird|
+        sum = sum + bird.review_count
+      end
+      sum
+    end
 end

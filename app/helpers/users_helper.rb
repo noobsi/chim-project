e046a4e2b654307862ced07cd18806383a4c60cc
BinhDV <dangbinh1996914@gmail.com>
2018-11-11 17:37:27 +0700

    add user profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,6 @@
 module UsersHelper
-  def avatar_for(user, options = { size: 300 })
-  	size = options[:size]
-  	if user.avatar?
-  		image_tag user.avatar.url(:thumb), class: 'avatar_image'
-  	else
-  		image_tag "anon.jpg", class: 'avatar_image'
-  	end
-  end
+	def image_tag(source, options={})
+		source = "anon.jpg" if source.blank?
+		super(source, options)
+	end
 end

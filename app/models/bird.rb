52064e6c43df8405592a295db6c7b78394ad9552
BinhDV <dangbinh1996914@gmail.com>
2018-11-27 12:19:23 +0700

    add chim

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -2,7 +2,6 @@ class Bird < ActiveRecord::Base
   belongs_to  :species
   has_many    :bird_images,   dependent: :destroy
   has_many    :reviews,       dependent: :destroy
-  has_many    :replies,       dependent: :destroy
 
   scope :by_name, ->(search){where("bird_name LIKE ?", "%#{search}%")}
 

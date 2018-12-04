f95525aa5b7a0baa704c3278072bbffc7131f224
Nguyen Tuan Anh <anhnt796@gmail.com>
2018-12-04 11:20:22 +0700

    Update rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -11,6 +11,6 @@ class Review < ActiveRecord::Base
   validates :rating_easy, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 50 }
 
   def rating_float
-    rating*0.1
+    ((rating+rating_price+rating_easy)*0.1/3).round(1)
   end
 end

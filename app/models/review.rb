cc688dab2dee118b6dd0b65ee39b6e5ea6d21f45
BinhDV <dangbinh1996914@gmail.com>
2018-12-04 11:05:18 +0700

    add multiple rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,6 +7,8 @@ class Review < ActiveRecord::Base
 
   validates :comment, presence: true
   validates :rating, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 50 }
+  validates :rating_price, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 50 }
+  validates :rating_easy, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 50 }
 
   def rating_float
     rating*0.1
